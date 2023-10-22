8a1091d2da8e313228dc7d18d132c6eac995978f
pp-qq <geometrylines2@gmail.com>
2023-10-23 02:03:46 +0900

    データ生成方法の追加

--- dataset/generate_cifar10.py
+++ dataset/generate_cifar10.py
@@ -1,60 +1,90 @@
+import json
 import os
+import pickle
 import random
 import sys
+from pathlib import Path
 
 import numpy as np
 import torch
 import torchvision
 import torchvision.transforms as transforms
-from utils.dataset_utils import check, save_file, separate_data, split_data
+from sklearn.model_selection import train_test_split
 
 random.seed(1)
 np.random.seed(1)
 num_clients = 20
 num_classes = 10
-dir_path = "Cifar10/"
+num_clusters = 5
+class_per_client = 3
+partission_method = "random"  # random, multi, prob
+
+dir_path = Path("Cifar10/")
 
 
-# Allocate data to users
-def generate_cifar10(
-    dir_path, num_clients, num_classes, niid, balance, partition
+def separate_data(
+    images,
+    labels,
+    num_clients,
+    num_classes,
+    class_per_client,
+    partission_method,
 ):
+    X = [[] for _ in range(num_clients)]
+    y = [[] for _ in range(num_clients)]
+
+    data_num_per_client = 5000
+
+    if partission_method == "random":
+        for i in range(num_clients):
+            for _ in range(data_num_per_client):
+                idx = np.random.randint(len(images))
+                X[i].append(images[idx])
+                y[i].append(labels[idx])
+        cluster_results = None
+
+    elif partission_method == "multi":
+        pass
+
+    elif partission_method == "prob":
+        pass
+
+    for i in range(num_clients):
+        X[i] = np.array(X[i])
+        y[i] = np.array(y[i])
+        print(f"Client {i}: {len(X[i])}")
+    print(X[0].shape)
+    print(y[0].shape)
+
+    return X, y, cluster_results
+
+
+def main():
     if not os.path.exists(dir_path):
         os.makedirs(dir_path)
 
     # Setup directory for train/test data
-    config_path = dir_path + "config.json"
-    train_path = dir_path + "train/"
-    test_path = dir_path + "test/"
+    train_path = dir_path / "train"
+    test_path = dir_path / "test"
+    if not os.path.exists(train_path):
+        os.makedirs(train_path)
+    if not os.path.exists(test_path):
+        os.makedirs(test_path)
 
-    if check(
-        config_path,
-        train_path,
-        test_path,
-        num_clients,
-        num_classes,
-        niid,
-        balance,
-        partition,
-    ):
-        return
-
-    # Get Cifar10 data
     transform = transforms.Compose(
         [
             transforms.ToTensor(),
-            transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+            transforms.Normalize((0.5,), (0.5,)),
         ]
     )
-
     trainset = torchvision.datasets.CIFAR10(
-        root=dir_path + "rawdata",
+        root=dir_path / "rawdata",
         train=True,
         download=True,
         transform=transform,
     )
     testset = torchvision.datasets.CIFAR10(
-        root=dir_path + "rawdata",
+        root=dir_path / "rawdata",
         train=False,
         download=True,
         transform=transform,
@@ -65,7 +95,6 @@ def generate_cifar10(
     testloader = torch.utils.data.DataLoader(
         testset, batch_size=len(testset.data), shuffle=False
     )
-
     for _, train_data in enumerate(trainloader, 0):
         trainset.data, trainset.targets = train_data
     for _, test_data in enumerate(testloader, 0):
@@ -81,44 +110,43 @@ def generate_cifar10(
     dataset_image = np.array(dataset_image)
     dataset_label = np.array(dataset_label)
 
-    # dataset = []
-    # for i in range(num_classes):
-    #     idx = dataset_label == i
-    #     dataset.append(dataset_image[idx])
+    print(type(dataset_image))
+    print(dataset_image.shape)
+    print(type(dataset_label))
+    print(dataset_label.shape)
 
     X, y, statistic = separate_data(
-        (dataset_image, dataset_label),
+        dataset_image,
+        dataset_label,
         num_clients,
         num_classes,
-        niid,
-        balance,
-        partition,
+        class_per_client,
+        partission_method,
     )
-    train_data, test_data = split_data(X, y)
-    save_file(
-        config_path,
-        train_path,
-        test_path,
-        train_data,
-        test_data,
-        num_clients,
-        num_classes,
-        statistic,
-        niid,
-        balance,
-        partition,
-    )
+
+    # Save train/test data
+    for i in range(num_clients):
+        X_train, X_test, y_train, y_test = train_test_split(
+            X[i], y[i], test_size=0.2, random_state=42
+        )
+
+        X_train = np.array(X_train)
+        X_test = np.array(X_test)
+        y_train = np.array(y_train)
+        y_test = np.array(y_test)
+
+        train_data = (X_train, y_train)
+        test_data = (X_test, y_test)
+
+        with open(train_path / f"train{i}.pkl", "wb") as f:
+            pickle.dump(train_data, f)
+        with open(test_path / f"test{i}.pkl", "wb") as f:
+            pickle.dump(test_data, f)
+
+    # Save statistic
+    with open(dir_path / "statistic.json", "w") as f:
+        json.dump(statistic, f, indent=4)
 
 
 if __name__ == "__main__":
-    # niid = True if sys.argv[1] == "noniid" else False
-    # balance = True if sys.argv[2] == "balance" else False
-    # partition = sys.argv[3] if sys.argv[3] != "-" else None
-
-    niid = True
-    balance = False
-    partition = "pat"  # "pat" pr "dir"
-
-    generate_cifar10(
-        dir_path, num_clients, num_classes, niid, balance, partition
-    )
+    main()
